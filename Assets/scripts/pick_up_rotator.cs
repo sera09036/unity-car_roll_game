8c0b603b580966df25bb41686b1c15094c9bca37
sera09036 <57556249+sera09036@users.noreply.github.com>
2020-02-13 18:03:37 +0800

    2nd commit

--- Assets/scripts/pick_up_rotator.cs
+++ Assets/scripts/pick_up_rotator.cs
@@ -7,7 +7,7 @@ public class pick_up_rotator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("回転開始");
+       // Debug.Log("回転開始");
     }
 
     // Update is called once per frame
