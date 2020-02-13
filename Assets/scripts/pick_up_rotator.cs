ae2def6cf6ecf05a38503820cb831d2dacf2926f
sera09036 <57556249+sera09036@users.noreply.github.com>
2020-02-13 17:08:36 +0800

    1st commit

---
+++ Assets/scripts/pick_up_rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pick_up_rotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("回転開始");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
